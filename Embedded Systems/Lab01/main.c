5280f4fad9bd18311df38c4046ea71935728c6e5
Nick Dominguez <nicolasmiami1999@gmail.com>
2026-09-03 11:54:42 -0400

    Completed Lab01

--- Embedded Systems/Lab01/main.c
+++ Embedded Systems/Lab01/main.c
@@ -1,31 +1,41 @@
-#include <msp430.h> 
-
-
-/**
- * main.c
- */
-int main(void)
-{
-	WDTCTL = WDTPW | WDTHOLD;	// stop watchdog timer
-	
-	return 0;
-}
-
-// Code that flashes the red LED
+// Code that delays the flashing red LED using _delay_cycles
 #include <msp430fr6989.h>
+#include <stdint.h>
 #define redLED BIT0 // Red LED at P1.0
+#define greenLED BIT7 // Green LED at P9.7
 void main(void) {
-	volatile unsigned int i;
+	volatile unsigned int i, j;
 	WDTCTL = WDTPW | WDTHOLD; 	// Stop the Watchdog timer
-	PM5CTL0 &= ~LOCKLPM5; 		// Disable GPIO power-on default high-
-		impedance mode
+	PM5CTL0 &= ~LOCKLPM5; 		// Disable GPIO power-on default high-impedance mode
 
 	P1DIR |= redLED; 	// Direct pin as output
 	P1OUT &= ~redLED; 	// Turn LED Off
-	for(;;) {
-		// Delay loop
-		for(i=0; i<20000; i++) {}
-
-		P1OUT ^= redLED; // Toggle the LED
+	P9DIR |= greenLED;	// Direct pin at output
+	P9OUT &= ~greenLED;  // Turn LED off
+	for(;;){
+		for(j=0; j<3; j++){
+			for(i=0; i<6; i++) {
+				// Delay loop
+				__delay_cycles(75000);
+               	P1OUT ^= redLED; // Toggle the LED
+			}
+			for(i=0; i<6; i++) {
+				// Delay loop
+				__delay_cycles(75000);
+                P9OUT ^= greenLED; // Toggle the LED
+			}
+		}
+		for(j=0; j<3; j++){
+			for(i=0; i<16; i++) {
+				// Delay loop
+				__delay_cycles(75000);
+               	P1OUT ^= redLED; // Toggle the LED
+			}
+			for(i=0; i<16; i++) {
+				// Delay loop
+				__delay_cycles(75000);
+                P9OUT ^= greenLED; // Toggle the LED
+			}
+		}
 	}
 }
