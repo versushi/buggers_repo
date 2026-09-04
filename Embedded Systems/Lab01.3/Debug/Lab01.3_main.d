59f137b9c2baab881cf412c91225d1f3c94ac152
Nick Dominguez <nicolasmiami1999@gmail.com>
2026-09-04 11:30:20 -0400

    Created problem 1.3

---
+++ Embedded Systems/Lab01.3/Debug/Lab01.3_main.d
@@ -0,0 +1,21 @@
+# FIXED
+
+Lab01.3_main.obj: ../Lab01.3_main.c
+Lab01.3_main.obj: C:/ti/ccs2101/ccs/ccs_base/msp430/include/msp430.h
+Lab01.3_main.obj: C:/ti/ccs2101/ccs/ccs_base/msp430/include/msp430fr6989.h
+Lab01.3_main.obj: C:/ti/ccs2101/ccs/ccs_base/msp430/include/in430.h
+Lab01.3_main.obj: C:/ti/ccs2101/ccs/tools/compiler/ti-cgt-msp430_21.6.3.LTS/include/intrinsics.h
+Lab01.3_main.obj: C:/ti/ccs2101/ccs/tools/compiler/ti-cgt-msp430_21.6.3.LTS/include/intrinsics_legacy_undefs.h
+
+../Lab01.3_main.c:
+
+C:/ti/ccs2101/ccs/ccs_base/msp430/include/msp430.h:
+
+C:/ti/ccs2101/ccs/ccs_base/msp430/include/msp430fr6989.h:
+
+C:/ti/ccs2101/ccs/ccs_base/msp430/include/in430.h:
+
+C:/ti/ccs2101/ccs/tools/compiler/ti-cgt-msp430_21.6.3.LTS/include/intrinsics.h:
+
+C:/ti/ccs2101/ccs/tools/compiler/ti-cgt-msp430_21.6.3.LTS/include/intrinsics_legacy_undefs.h:
+
