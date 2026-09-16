66b5fbd6c2e39af493cbaf478cf85a897f004d32
Nick Dominguez <nicolasmiami1999@gmail.com>
2026-09-15 20:25:46 -0400

    Created Lab03.2

---
+++ Embedded Systems/Lab03/Lab03.2/Debug/Lab03.2_main.d
@@ -0,0 +1,51 @@
+# FIXED
+
+Lab03.2_main.obj: ../Lab03.2_main.c
+Lab03.2_main.obj: C:/ti/ccs2101/ccs/ccs_base/msp430/include/msp430.h
+Lab03.2_main.obj: C:/ti/ccs2101/ccs/ccs_base/msp430/include/msp430fr6989.h
+Lab03.2_main.obj: C:/ti/ccs2101/ccs/ccs_base/msp430/include/in430.h
+Lab03.2_main.obj: C:/ti/ccs2101/ccs/tools/compiler/ti-cgt-msp430_21.6.3.LTS/include/intrinsics.h
+Lab03.2_main.obj: C:/ti/ccs2101/ccs/tools/compiler/ti-cgt-msp430_21.6.3.LTS/include/intrinsics_legacy_undefs.h
+Lab03.2_main.obj: C:/ti/ccs2101/ccs/tools/compiler/ti-cgt-msp430_21.6.3.LTS/include/stdint.h
+Lab03.2_main.obj: C:/ti/ccs2101/ccs/tools/compiler/ti-cgt-msp430_21.6.3.LTS/include/_ti_config.h
+Lab03.2_main.obj: C:/ti/ccs2101/ccs/tools/compiler/ti-cgt-msp430_21.6.3.LTS/include/linkage.h
+Lab03.2_main.obj: C:/ti/ccs2101/ccs/tools/compiler/ti-cgt-msp430_21.6.3.LTS/include/_stdint40.h
+Lab03.2_main.obj: C:/ti/ccs2101/ccs/tools/compiler/ti-cgt-msp430_21.6.3.LTS/include/sys/stdint.h
+Lab03.2_main.obj: C:/ti/ccs2101/ccs/tools/compiler/ti-cgt-msp430_21.6.3.LTS/include/sys/cdefs.h
+Lab03.2_main.obj: C:/ti/ccs2101/ccs/tools/compiler/ti-cgt-msp430_21.6.3.LTS/include/sys/_types.h
+Lab03.2_main.obj: C:/ti/ccs2101/ccs/tools/compiler/ti-cgt-msp430_21.6.3.LTS/include/machine/_types.h
+Lab03.2_main.obj: C:/ti/ccs2101/ccs/tools/compiler/ti-cgt-msp430_21.6.3.LTS/include/machine/_stdint.h
+Lab03.2_main.obj: C:/ti/ccs2101/ccs/tools/compiler/ti-cgt-msp430_21.6.3.LTS/include/sys/_stdint.h
+
+../Lab03.2_main.c:
+
+C:/ti/ccs2101/ccs/ccs_base/msp430/include/msp430.h:
+
+C:/ti/ccs2101/ccs/ccs_base/msp430/include/msp430fr6989.h:
+
+C:/ti/ccs2101/ccs/ccs_base/msp430/include/in430.h:
+
+C:/ti/ccs2101/ccs/tools/compiler/ti-cgt-msp430_21.6.3.LTS/include/intrinsics.h:
+
+C:/ti/ccs2101/ccs/tools/compiler/ti-cgt-msp430_21.6.3.LTS/include/intrinsics_legacy_undefs.h:
+
+C:/ti/ccs2101/ccs/tools/compiler/ti-cgt-msp430_21.6.3.LTS/include/stdint.h:
+
+C:/ti/ccs2101/ccs/tools/compiler/ti-cgt-msp430_21.6.3.LTS/include/_ti_config.h:
+
+C:/ti/ccs2101/ccs/tools/compiler/ti-cgt-msp430_21.6.3.LTS/include/linkage.h:
+
+C:/ti/ccs2101/ccs/tools/compiler/ti-cgt-msp430_21.6.3.LTS/include/_stdint40.h:
+
+C:/ti/ccs2101/ccs/tools/compiler/ti-cgt-msp430_21.6.3.LTS/include/sys/stdint.h:
+
+C:/ti/ccs2101/ccs/tools/compiler/ti-cgt-msp430_21.6.3.LTS/include/sys/cdefs.h:
+
+C:/ti/ccs2101/ccs/tools/compiler/ti-cgt-msp430_21.6.3.LTS/include/sys/_types.h:
+
+C:/ti/ccs2101/ccs/tools/compiler/ti-cgt-msp430_21.6.3.LTS/include/machine/_types.h:
+
+C:/ti/ccs2101/ccs/tools/compiler/ti-cgt-msp430_21.6.3.LTS/include/machine/_stdint.h:
+
+C:/ti/ccs2101/ccs/tools/compiler/ti-cgt-msp430_21.6.3.LTS/include/sys/_stdint.h:
+
---
+++ Embedded Systems/Lab03/Lab03.2/Lab03.2_main.c
@@ -0,0 +1,53 @@
+#include <msp430.h> 
+#include  <stdint.h>
+
+
+// Declare function
+void config_ACLK_to_32KHz_crystal();
+
+// Flashing the LED with Timer_A, continuous mode, via polling
+#define redLED BIT0 // Red LED at P1.0
+#define greenLED BIT7 // Green LED at P9.7
+void main(void) {
+// Stop the Watchdog timer & Unlock the GPIO pins
+	WDTCTL = WDTPW | WDTHOLD;
+	PM5CTL0 &= ~LOCKLPM5;
+// Configure the LEDs as output
+	P1DIR |= redLED;		// Direct pin 1 as output
+	P9DIR |= greenLED; 		// Direct pin 9 as output
+	P1OUT &= ~redLED; 		// Turn LED off
+	P9OUT &= ~greenLED; 	// Turn LED off
+// Configure ACLK to the 32 KHz crystal (function call)
+	config_ACLK_to_32KHz_crystal();
+// Configure Timer_A
+// Use ACLK, divide by 1, up mode, clear TAR
+	TA0CTL =  TASSEL_1 | ID_0 | MC_1 | TACLR;
+// Ensure flag is cleared at the start
+	TA0CTL &= ~TAIFG;
+	TA0CCR0 = 32768 - 1;	// Sets the target frequency
+// Infinite loop
+	for(;;) {
+// Wait in this empty loop for the flag to raise 
+		while((TA0CTL & TAIFG) != TAIFG){} // delay
+// Do the action here
+		P1OUT ^= redLED; 	// toggle redLED
+		TA0CTL &= ~TAIFG;	// Resets flag
+	}
+}
+
+// Configures ACLK to 32 KHz crystal
+void config_ACLK_to_32KHz_crystal() {
+// By default, ACLK runs on LFMODCLK at 5MHz/128 = 39 KHz
+// Reroute pins to LFXIN/LFXOUT functionality
+	PJSEL1 &= ~BIT4;
+	PJSEL0 |= BIT4;
+	// Wait until the oscillator fault flags remain cleared
+	CSCTL0 = CSKEY; // Unlock CS registers
+	do {
+		CSCTL5 &= ~LFXTOFFG; // Local fault flag
+		SFRIFG1 &= ~OFIFG; // Global fault flag
+	}
+	while((CSCTL5 & LFXTOFFG) != 0);
+		CSCTL0_H = 0; // Lock CS registers
+	return;
+}
